d7db17096719b3ec12fee34d366c380fd9309acc
Fineas Anton <fineas.anton@gmail.com>
2025-01-29 14:56:21 +0200

    Wip

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -57,32 +57,13 @@ const period = ref<Period>('daily')
 
       <UDashboardToolbar>
         <template #left>
-          <!-- ~/components/home/HomeDateRangePicker.vue -->
-          <HomeDateRangePicker
-            v-model="range"
-            class="-ml-2.5"
-          />
-
-          <!-- ~/components/home/HomePeriodSelect.vue -->
-          <HomePeriodSelect
-            v-model="period"
-            :range="range"
-          />
         </template>
       </UDashboardToolbar>
 
       <UDashboardPanelContent>
-        <!-- ~/components/home/HomeChart.vue -->
-        <HomeChart
-          :period="period"
-          :range="range"
-        />
 
         <div class="grid lg:grid-cols-2 lg:items-start gap-8 mt-8">
-          <!-- ~/components/home/HomeSales.vue -->
-          <HomeSales />
-          <!-- ~/components/home/HomeCountries.vue -->
-          <HomeCountries />
+
         </div>
       </UDashboardPanelContent>
     </UDashboardPanel>
